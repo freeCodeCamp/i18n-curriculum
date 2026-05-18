a40ed6e1fb9054b2d9acbefb3250982c5810b659
DanielRosa74 <58576743+DanielRosa74@users.noreply.github.com>
2026-05-18 20:55:21 -0300

    feat(i18n, learn): script changes

--- scripts/move-bf.sh
+++ scripts/move-bf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Define the source file
-source_file="scripts/59e0a8df964e4540d5abe599.md"
+# # Define the source file
+# source_file="scripts/59e0a8df964e4540d5abe599.md"
 
-# Define the destination directory
-destination_dir="curriculum/challenges"
+# # Define the destination directory
+# destination_dir="curriculum/challenges"
 
-# Loop through only the specified language directories
-for lang in german swahili; do
-    cp "./$source_file" "./$destination_dir/$lang/blocks/rosetta-code-challenges/"
-done
+# # Loop through only the specified language directories
+# for lang in german swahili; do
+#     cp "./$source_file" "./$destination_dir/$lang/blocks/rosetta-code-challenges/"
+# done
